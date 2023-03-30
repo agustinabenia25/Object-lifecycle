0d07fb5ab67523f144b9ca6580a929c0a69b5359
agustinabenia25 <agustina.benia@correo.ucu.edu.uy>
2023-03-30 02:51:47 +0100

    Completo ejercicio ciclo de vida de objetos

--- src/ClassLibrary/Train.cs
+++ src/ClassLibrary/Train.cs
@@ -8,52 +8,71 @@ using System;
 
 namespace ClassLibrary
 {
-    /// <summary>
-    /// Esta clase representa un tren muy básico.
-    /// </summary>
+    // Esta clase representa un tren muy básico.
     public class Train
     {
-        /// <summary>
-        /// Obtiene un valor que indica si las maquinas del tren han sido encendidas o no.
-        /// </summary>
-        /// <value><c>true</c> si las máquinas fueron encendidas, <c>false</c> en caso contrario.</value>
+        // Obtiene un valor que indica si las maquinas del tren han sido encendidas o no.
+        // true si las máquinas fueron encendidas, false en caso contrario.
         public bool IsEngineStarted { get; private set; }
-
-        /// <summary>
-        /// Enciende las máquinas del tren.
-        /// </summary>
-        /// <returns>
-        /// <c>true</c> si las máquinas pueden ser encendidas, <c>false</c> en caso contrario.
-        /// </returns>
+        // Creo la variable de clase count:
+        private static int count = 0;
+        // Creo la variable de instancia id, quien va a ser mi identificador:
+        public string id;
+        // Defino el constructor de la clase:
+        public Train(string id)
+        {
+            this.id = id;
+            // Cada vez que se cree una instancia, el contador va a aumentar:
+            Train.count = Train.count + 1;
+            //Console.WriteLine("Un tren fue creado");
+        }
+        // Defino un metodo para determinar el identificador de un tren:
+        public void SetId(string value)
+        {
+            this.id = value;
+        }
+        // Defino un metodo para obtener el identificador de un tren:
+        public string GetId()
+        {
+            return this.id;
+        }
+        // Defino un metodo para descubrir cuantos trenas hay creados hasta el momento:
+        public static int GetCount()
+        {
+            return Train.count;
+        }
+        // Defino el destructor de la clase:
+        ~Train()
+        {
+            Train.count--;
+            //Console.WriteLine("Un tren fue destruido");
+        }
+        // Enciende las maquinas del tren.
+        // Devuelve true si las maquinas fueron encendidas, false en caso contrario.
         public bool StartEngines()
         {
             if (this.IsEngineStarted)
             {
-                Console.Write("The engines are already running");
+                Console.Write("The engines are already running \n");
                 return false;
             }
 
             this.IsEngineStarted = true;
-            Console.Write("Engines on");
+            Console.Write($"{this.id}. Engines on \n");
             return true;
         }
-
-        /// <summary>
-        /// Detiene las máquinas del tren.
-        /// </summary>
-        /// <returns>
-        /// <c>true</c> si las máquinas pueden ser detenidas, <c>false</c> en caso contrario.
-        /// </returns>
+        // Detiene las maquinas del tren.
+        // Devuelve true si las maquinas pueden ser detenidas, false en caso contrario.
         public bool StopEngines()
         {
             if (this.IsEngineStarted)
             {
                 this.IsEngineStarted = false;
-                Console.Write("Engines off");
+                Console.Write($"{this.id}. Engines off \n");
                 return true;
             }
 
-            Console.Write("The engines are already stopped");
+            Console.Write("The engines are already stopped \n");
             return this.IsEngineStarted;
         }
     }
--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -19,9 +19,30 @@ namespace ConsoleApplication
         /// </summary>
         public static void Main()
         {
-            var train = new Train();
-            train.StartEngines();
+            for (int x = 1; x <= 25000; x++)
+            {
+                // Creo 1000 instancias de la clase Train 
+                var tren = new Train("");
+            }
+
+            var train1 = new Train("Last Train to London");
+            var train2 = new Train("Last Train to London");
+            var train3 = new Train("Runaway Train");
+            train1.StartEngines();
             Console.WriteLine("Hello World!");
+            // Imprimo cuantos trenes hay vivos hasta el momento
+            Console.WriteLine($"En este momento hay {Train.GetCount()} objetos de la clase Train en la memoria.");
+            Console.WriteLine($"Las variables train1 y train2, ¿apuntan al mismo objeto? {train1 == train2}");
+            Console.WriteLine($"Las variables train2 y train3, ¿apuntan al mismo objeto? {train2 == train3}");
+            train1.StopEngines();
         }
     }
 }
+/*
+Las tres variables train1, train2 y train3 son 3 instancias distintas de la clase Train.
+
+Por lo tanto, si bien train1 y train 2 son iguales, porque en ambos casos la propiedad Id contiene el valor "Last Train to London",
+al evaluar {train1 == train2} el resultado va a ser False. Esto es porque los objetos referenciados por cada variable son diferentes, son dos
+objetos distintos, cada uno ocupa un espacio diferente en el heap de la memoria. Esta explicación también justifica el resultado de la comparación
+{train1 == train2}, aunque es más intuitivo entender porque en este caso la respuesta también es False.
+*/
